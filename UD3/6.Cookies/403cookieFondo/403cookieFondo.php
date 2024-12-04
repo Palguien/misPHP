e9830bf3a0b841ef49fd03b4700cd822e8fd6e41
Pablo Pollos <pablo.poligl.1@educa.jcyl.es>
2024-12-04 14:00:18 +0100

    04/12/2024

--- UD3/6.Cookies/403cookieFondo/403cookieFondo.php
+++ UD3/6.Cookies/403cookieFondo/403cookieFondo.php
@@ -5,30 +5,38 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
     <style>
-        .oscuro{
-            background-color: black;
-            color: white;
+        a{
+            text-decoration: none;
+            color: black;
+        }
+        button:hover{
+            background-color:gray;
+            cursor: pointer;
         }
     </style>
 </head>
 
 <?php
     $nombrecookie = 'visitas';
+    $clase;
 
     if(!isset($_COOKIE['fondo'])){
         setcookie('fondo', 'claro', time()+3600*48);
         echo "Bienvenido por primera vez";
     }else{
+        if($_COOKIE['fondo']=='oscuro'){
+            echo "<style>body{background-color: black; color:white;}</style>";
+        }else{
+            echo "<style>body{background-color: antiquewhite; color:grey;}</style>";
+        }
         echo "Bienvenido";
     }
-
 ?>
 
-<body class="<?= $_COOKIE['fondo']?>" >
+<body>
     <br>
-    <a href="oscuro.php">Modo oscuro</a><br>
-    <a href="claro.php">Modo claro</a><br>
-    <a href="borrarCookie.php">Borrar cookies</a><br>
-
+    <button><a href="oscuro.php">Modo oscuro</a></button><br>
+    <button><a href="claro.php">Modo claro</a></button><br>
+    <button><a href="borrarCookie.php">Borrar cookies</a></button><br>
 </body>
 </html>
